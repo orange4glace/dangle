c17002b03f1d7d0359d1209e96878fcf372108dd
orange4glace <orange4glace@gmail.com>
2020-07-18 00:41:52 +0900

    add: dangling state

--- lib/dangle.ts
+++ lib/dangle.ts
@@ -20,14 +20,17 @@ interface DangleMouseEvent {
 export interface DangleOption {
   holdingTheshold: number;
   holdingOrthogonalTheshold: number;
+  flickThreshold: number;
   stretch: number;
   strecthThreshold: number;
   minStep: number;
   maxStep: number;
   padding: number;
   interactable: boolean;
+  danglingDuration: number;
   mapEventToPosition: (e: DangleMouseEvent) => number;
   mapEventToOrthogonalPosition: (e: DangleMouseEvent) => number;
+  danglingEasingFunction: (t: number) => number;
 }
 
 export class Dangle {
@@ -40,6 +43,19 @@ export class Dangle {
   private lastOrthogonalPosition_: number;
 
   private dpStack_: Array<{time: number, dp: number}> = [];
+
+  private currentStep_: number;
+  private currentValue_: number;
+
+  private holdingStartValue_: number;
+  private holdingStartPosition_: number;
+  private holdingStartOrthogonalPosition_: number;
+
+  private danglingStartValue_: number;
+  private danglingEndValue_: number;
+  private danglingTargetStep_: number;
+  private currentDanglingDuration_: number;
+  private currentDanglingEasingFunction_: (t: number) => number;
 
   constructor(
     private readonly el_: HTMLElement,
@@ -61,9 +77,13 @@ export class Dangle {
     this.el_.addEventListener('touchstart', this.startCheckHoldingThreshold);
   }
 
+  private setState(state: DangleState) {
+    this.state_ = state;
+  }
+
   private startCheckHoldingThreshold(evt: MouseEvent | TouchEvent) {
-    if (this.state_ !== DangleState.IDLE) return;
     if (!this.option_.interactable) return;
+    if (this.state_ !== DangleState.IDLE) return;
     const e = toDangleMouseEvent(evt);
     this.lastPosition_ = this.option_.mapEventToPosition(e);
     this.lastOrthogonalPosition_ = this.option_.mapEventToOrthogonalPosition(e);
@@ -102,8 +122,10 @@ export class Dangle {
     const e = toDangleMouseEvent(evt);
     this.lastPosition_ = this.option_.mapEventToPosition(e);
     this.lastOrthogonalPosition_ = this.option_.mapEventToOrthogonalPosition(e);
+    this.holdingStartValue_ = this.currentValue_;
+    this.holdingStartPosition_ = this.lastPosition_;
+    this.holdingStartOrthogonalPosition_ = this.lastOrthogonalPosition_;
     this.dpStack_ = [];
-    this.state_ = DangleState.HOLDING;
     window.addEventListener('mousemove', this.holding);
     window.addEventListener('touchmove', this.holding);
     const mouseup = () => {
@@ -115,6 +137,7 @@ export class Dangle {
     };
     window.addEventListener('mouseup', mouseup);
     window.addEventListener('touchend', mouseup);
+    this.setState(DangleState.HOLDING);
   }
 
   private holding(evt: MouseEvent | TouchEvent) {
@@ -129,12 +152,37 @@ export class Dangle {
       dp: dp,
     });
     if (this.dpStack_.length > 10) this.dpStack_.length = 10;
+    const dv = pos / this.option_.stretch;
+    const nextValue = this.holdingStartValue_ + dv;
+    this.setValue(nextValue);
   }
 
   private startDangling(evt: MouseEvent | TouchEvent) {
     if (this.state_ !== DangleState.HOLDING) return;
     this.lastTime_ = 0;
+    const now = Date.now();
+    const dpCheckTimeDiff = 100;
+    let dpSum = 0;
+    this.dpStack_.forEach(dp => {
+      if (now - dp.time < dpCheckTimeDiff) {
+        dpSum += dp.dp;
+      }
+    });
+    let danglingTargetStep: number;
+    let dpSign = dpSum >= 0 ? 1 : -1;
+    if (dpSum > this.option_.flickThreshold) {
+      danglingTargetStep = this.currentStep_ + 1 * dpSign;
+    }
+    else {
+      danglingTargetStep = Math.round(Math.abs(this.currentValue_)) * dpSign;
+    }
+    this.danglingTargetStep_ = danglingTargetStep;
+    this.danglingStartValue_ = this.currentValue_;
+    this.danglingEndValue_ = this.stepToValue(this.danglingTargetStep_);
+    this.currentDanglingEasingFunction_ = this.option_.danglingEasingFunction;
+    this.currentDanglingDuration_ = this.option_.danglingDuration;
     requestAnimationFrame(this.dangling);
+    this.setState(DangleState.DANGLING);
   }
 
   private dangling(time: number) {
@@ -143,9 +191,40 @@ export class Dangle {
       requestAnimationFrame(this.dangling);
       return;
     }
-    const dt = time - this.lastTime_;
+    const dt = Math.min(1, (time - this.lastTime_) / this.currentDanglingDuration_);
     this.lastTime_ = time;
+    const easing = this.currentDanglingEasingFunction_(dt);
+    const nextValue = (this.danglingEndValue_ - this.danglingStartValue_) * easing + this.danglingStartValue_;
+    this.setValue(nextValue);
+    if (dt >= 1) {
+      this.setState(DangleState.IDLE);
+      return;
+    }
     requestAnimationFrame(this.dangling);
+  }
+
+  private setValue(value: number): void {
+    this.currentValue_ = this.clampValue(value);
+    const sign = value >= 0 ? 1 : -1;
+    this.currentStep_ = this.clampStep(Math.floor(Math.abs(value)) * sign);
+  }
+
+  private valueToStep(value: number): number {
+    return value;
+  }
+
+  private stepToValue(step: number): number {
+    return step;
+  }
+
+  private clampValue(value: number): number {
+    const minValue = this.valueToStep(this.option_.minStep);
+    const maxValue = this.valueToStep(this.option_.maxStep);
+    return Math.max(minValue, Math.min(maxValue, value));
+  }
+
+  private clampStep(value: number): number {
+    return Math.max(this.option_.minStep, Math.min(this.option_.maxStep, value));
   }
 
   public dispose() {
